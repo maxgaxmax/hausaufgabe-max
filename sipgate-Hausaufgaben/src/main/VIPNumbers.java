f3b64be28a8e55db3cc8bfd9af01419e9808711b
Max Rievers <35071360+maxgaxmax@users.noreply.github.com>
2018-12-04 22:56:42 +0100

    Update VIPNumbers.java

--- sipgate-Hausaufgaben/src/main/VIPNumbers.java
+++ sipgate-Hausaufgaben/src/main/VIPNumbers.java
@@ -3,35 +3,32 @@ import java.util.Arrays; //Braucht man vermutlich nicht
 public class VIPNumbers {
     public static void main (String [] args) {
         
-        int startNum;
-        String comma = "";
-        String[] Array = {"0211/13571357", "0201/963421", "0211/6300553", "0203/3002020","0201/23465211", "0211/6433555", "0203/0211031", "0211/11223344", "0211/1230321", "0211/2843261"};
-        System.out.print("These are the VIP phone numbers:");
+        String komma = "";
+        String[] Array = {"0211/1357", "0201/963421", "0211/6300553", "0203/3002020","0201/23465211", "0211/6433555", "0203/0211031", "0211/11223344", "0211/1230321", "0211/2843261"};      //Hier stehen die zu überprüfenden Telefonnummern 
+        System.out.print("These are the VIP phone numbers:");                       //Dieser Text soll vor den VIP-Telefonnummern stehen
         
-        for (int o = 0; o < 10; o++){
+        for (int positionImArray = 0; positionImArray < 10; positionImArray++){     //for-Schleife um die entsprechende Zahl im Array zu benutzten
             
-            int length = Array[o].substring(5).length();
+            int laenge = Array[positionImArray].substring(5).length();              //Hier wird geschaut, wie lang die Telefonnummer ohne Vorwahl ist
             boolean stop = false;
-            
-            for (int a = 0; a < length - 1; a++){
-              
-                int k = 1;
-                if (!stop){
-                    for (int i = 0; i < (length - a) / 2; i++) {
-                        if (!stop){
-                            String newNum = Array[o].substring(5).substring(a, i + a + 1);
-                            int num = Integer.parseInt(newNum);
-                            startNum = num;
-                            String recurrentNum =  newNum + newNum;
-                            num++;
-                            newNum = newNum + Integer.toString(num);
 
-                            if (Array[o].substring(5).contains(newNum) || Array[o].substring(5).contains(recurrentNum)){
+            for (int a = 0; a < laenge - 1; a++){                                   //Diese for-Schleife laeuft für jede Ziffer einmal durch 
+              
+                if (!stop){                                                         //Nummer wird nicht noch weiter überprüft, wenn es sich um eine VIP-Nummer handelt
+                    
+                    for (int i = 0; i < (laenge - a) / 2; i++) {                                                                                //In dieser Gesamten for-Schleife
+                                                                                                                                                //wird abgefragt wie lang die in
+                        String newNum = Array[positionImArray].substring(5).substring(a, i + a + 1);                                            //der Telefonnummer enthaltenen
+                        int num = Integer.parseInt(newNum);                                                                                     //Zahlen lang seien dürfen und
+                        String recurrentNum =  newNum + newNum;                                                                                 //es wird die für die Aufgaben-
+                        num++;                                                                                                                  //stellung nötige Rechnung
+                        newNum = newNum + Integer.toString(num);                                                                                //ausgeführt.
+                                                                                                                                                
+                        if (Array[positionImArray].substring(5).contains(newNum) || Array[positionImArray].substring(5).contains(recurrentNum)){//prüft ob es sich um eine VIP-Telefonnummer handelt
                                 
-                                System.out.print(comma + " " + Array[o]);
-                                comma = ",";
-                                stop = true;
-                            }
+                            System.out.print(komma + " " + Array[positionImArray]); //Gibt die Vip-Telefonnummern aus
+                            komma = ",";
+                            stop = true;
                         }
                     }
                 }
